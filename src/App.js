4bc7d873aca4ed67edddc3d8ecc0a68012e9e495
SanaUllahRasheed123 <starsreachersacademy@gmail.com>
2023-10-24 15:56:52 +0500

    destructuringOfArrays

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 // import { Axios } from 'axios'
 import React from 'react'
 // import AxiosTut from './AxiosTut'
-import AxiosPost from './AxiosPost'
+// import AxiosPost from './AxiosPost'
+import Name from './Name'
 
 const App = () => {
+  // const arr=[1,2,3,,5,6,7]
+
+  // const [one,two,three,,five,six,seven]=arr
+
+  // console.log(one,two,three,five,six,seven)
   return (
     <div>
       {/* <AxiosTut /> */}
-      <AxiosPost />
+      {/* <AxiosPost /> */}
+
+    <Name fname="Sana" lname="Ullah"/>
     </div>
   )
 }
---
+++ src/Name.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Name = ({fname},{lname}) => {
+    // const [fname,lname]= props;
+
+  return (
+    <>
+      <div>{fname}{lname}</div>
+    </>
+  )
+}
+
+export default Name
