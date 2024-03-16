00a1fc83c2ff382ec9360d813cf4e3d9984ec1b6
SanaUllahRasheed123 <starsreachersacademy@gmail.com>
2024-03-16 23:57:09 +0500

    working

--- src/App.js
+++ src/App.js
@@ -1,22 +1,44 @@
-// import { Axios } from 'axios'
+// // import { Axios } from 'axios'
+// import React from 'react'
+// // import AxiosTut from './AxiosTut'
+// // import AxiosPost from './AxiosPost'
+// import Name from './Name'
+
+// const App = () => {
+//   // const arr=[1,2,3,,5,6,7]
+
+//   // const [one,two,three,,five,six,seven]=arr
+
+//   // console.log(one,two,three,five,six,seven)
+//   return (
+//     <div>
+//       {/* <AxiosTut /> */}
+//       {/* <AxiosPost /> */}
+
+//     <Name fname="Sana" lname="Ullah"/>
+//     </div>
+//   )
+// }
+
+// export default App
+
+
 import React from 'react'
-// import AxiosTut from './AxiosTut'
-// import AxiosPost from './AxiosPost'
-import Name from './Name'
 
 const App = () => {
-  // const arr=[1,2,3,,5,6,7]
+  const z = 2;
 
-  // const [one,two,three,,five,six,seven]=arr
-
-  // console.log(one,two,three,five,six,seven)
+  
   return (
-    <div>
-      {/* <AxiosTut /> */}
-      {/* <AxiosPost /> */}
-
-    <Name fname="Sana" lname="Ullah"/>
+    <>
+    <div style={{textAlign:'center', fontSize:'sans serif'}}>
+    <h1>Hello The value of varible is {3+5}</h1>
+    <h2>Hrllo</h2>
+    <p>{(z)<5?'Greater':'Smaller'}</p>
     </div>
+       
+    </>
+  
   )
 }
 
