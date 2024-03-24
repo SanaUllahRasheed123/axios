5b7fe89ed884ae61a238ca7d157131d7837b3298
SanaUllahRasheed123 <starsreachersacademy@gmail.com>
2024-03-24 19:24:24 +0500

    updated

--- src/App.js
+++ src/App.js
@@ -42,20 +42,20 @@
 
 // export default App
 
-import React from "react";
+// import React from "react";
 
-import { ToastContainer, toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+// import { ToastContainer, toast } from "react-toastify";
+// import "react-toastify/dist/ReactToastify.css";
 
-function App() {
-  const notify = () => toast("Wow Sana Ullah How are you!");
+// function App() {
+//   const notify = () => toast("Wow Sana Ullah How are you!");
 
-  return (
-    <div>
-      <button onClick={notify}>Notify!</button>
-      <ToastContainer />
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       <button onClick={notify}>Notify!</button>
+//       <ToastContainer />
+//     </div>
+//   );
+// }
 
-export default App;
+// export default App;
